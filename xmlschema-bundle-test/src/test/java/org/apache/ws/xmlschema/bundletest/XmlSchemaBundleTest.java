684c5cc39e7b6fe5e054b6f451560a127490bab6
Daniel Kulp <dkulp@apache.org>
2014-09-11 16:42:22 +0000

    Update to a newer version of pax-exam

--- xmlschema-bundle-test/src/test/java/org/apache/ws/xmlschema/bundletest/XmlSchemaBundleTest.java
+++ xmlschema-bundle-test/src/test/java/org/apache/ws/xmlschema/bundletest/XmlSchemaBundleTest.java
@@ -20,29 +20,30 @@
 package org.apache.ws.xmlschema.bundletest;
 
 import junit.framework.Assert;
+
 import org.apache.ws.commons.schema.XmlSchemaCollection;
 import org.apache.ws.commons.schema.internal.DummyInternalClass;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-
+import org.ops4j.pax.exam.junit.PaxExam;
 
 /**
  *
  */
-@RunWith(JUnit4TestRunner.class)
+@RunWith(PaxExam.class)
 public class XmlSchemaBundleTest {
 
     @Configuration
     public static Option[] configuration()
     {
-       return CoreOptions.options(CoreOptions.felix(), CoreOptions.provision(
-         CoreOptions.mavenBundle().groupId("org.apache.ws.xmlschema").artifactId("xmlschema-core")
+       return CoreOptions.options(
+            CoreOptions.junitBundles(),
+            CoreOptions.provision(CoreOptions.mavenBundle().groupId("org.apache.ws.xmlschema").artifactId("xmlschema-core")
        ));
     }
 
