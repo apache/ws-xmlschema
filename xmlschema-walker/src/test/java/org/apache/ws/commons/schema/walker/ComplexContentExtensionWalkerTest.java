284c0fa017771222740fb83f1cb9696163de9e1c
Colm O hEigeartaigh <coheigea@apache.org>
2026-09-24 11:47:42 +0100

    Allow an xs:all alongside an empty particle in a complex content extension

--- xmlschema-walker/src/test/java/org/apache/ws/commons/schema/walker/ComplexContentExtensionWalkerTest.java
+++ xmlschema-walker/src/test/java/org/apache/ws/commons/schema/walker/ComplexContentExtensionWalkerTest.java
@@ -19,6 +19,7 @@
 package org.apache.ws.commons.schema.walker;
 
 import java.io.StringReader;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,6 +42,31 @@ import org.junit.Test;
 public class ComplexContentExtensionWalkerTest extends Assert {
 
     private static List<String> walkRoot(String body) {
+        // Records the namespace of every attribute wildcard visited.
+        final List<String> anyAttributeNamespaces = new ArrayList<String>();
+        walk(body, (proxy, method, args) -> {
+            if ("onVisitAnyAttribute".equals(method.getName())) {
+                anyAttributeNamespaces.add(((XmlSchemaAnyAttribute)args[1]).getNamespace());
+            }
+            return null;
+        });
+        return anyAttributeNamespaces;
+    }
+
+    /**
+     * Walks the root element and returns the name of every visitor callback,
+     * in order.
+     */
+    private static List<String> walkEvents(String body) {
+        final List<String> events = new ArrayList<String>();
+        walk(body, (proxy, method, args) -> {
+            events.add(method.getName());
+            return null;
+        });
+        return events;
+    }
+
+    private static void walk(String body, InvocationHandler handler) {
         XmlSchemaCollection collection = new XmlSchemaCollection();
         collection.read(new StringReader(
             "<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:tns=\"urn:ext\""
@@ -50,18 +76,9 @@ public class ComplexContentExtensionWalkerTest extends Assert {
         XmlSchemaElement root = collection.getElementByQName(new QName("urn:ext", "root"));
         assertNotNull("the schema under test must declare a 'root' element", root);
 
-        // Records the namespace of every attribute wildcard visited.
-        final List<String> anyAttributeNamespaces = new ArrayList<String>();
         XmlSchemaVisitor visitor = (XmlSchemaVisitor)Proxy.newProxyInstance(
-            XmlSchemaVisitor.class.getClassLoader(), new Class<?>[] {XmlSchemaVisitor.class},
-            (proxy, method, args) -> {
-                if ("onVisitAnyAttribute".equals(method.getName())) {
-                    anyAttributeNamespaces.add(((XmlSchemaAnyAttribute)args[1]).getNamespace());
-                }
-                return null;
-            });
+            XmlSchemaVisitor.class.getClassLoader(), new Class<?>[] {XmlSchemaVisitor.class}, handler);
         new XmlSchemaWalker(collection, visitor).walk(root);
-        return anyAttributeNamespaces;
     }
 
     @Test
@@ -79,6 +96,70 @@ public class ComplexContentExtensionWalkerTest extends Assert {
         } catch (XmlSchemaException expected) {
             assertTrue(expected.getMessage(), expected.getMessage().contains("xs:all"));
         }
+    }
+
+    /**
+     * An empty particle adds no content, so XML Schema allows it alongside an
+     * xs:all: the xs:all is then the whole content model.
+     */
+    @Test
+    public void testExtendingAnAllGroupWithAnEmptySequenceWalks() {
+        assertAllGroupWalked("<xs:complexType name=\"base\"><xs:all>"
+                             + "<xs:element name=\"a\" type=\"xs:string\"/>"
+                             + "<xs:element name=\"b\" type=\"xs:string\"/>"
+                             + "</xs:all></xs:complexType>"
+                             + "<xs:complexType name=\"derived\"><xs:complexContent>"
+                             + "<xs:extension base=\"tns:base\"><xs:sequence/>"
+                             + "<xs:attribute name=\"id\" type=\"xs:string\"/>"
+                             + "</xs:extension></xs:complexContent></xs:complexType>"
+                             + "<xs:element name=\"root\" type=\"tns:derived\"/>");
+    }
+
+    @Test
+    public void testExtendingAnAllGroupWithAChoiceThatCannotOccurWalks() {
+        assertAllGroupWalked("<xs:complexType name=\"base\"><xs:all>"
+                             + "<xs:element name=\"a\" type=\"xs:string\"/>"
+                             + "</xs:all></xs:complexType>"
+                             + "<xs:complexType name=\"derived\"><xs:complexContent>"
+                             + "<xs:extension base=\"tns:base\">"
+                             + "<xs:choice minOccurs=\"0\" maxOccurs=\"0\"/>"
+                             + "</xs:extension></xs:complexContent></xs:complexType>"
+                             + "<xs:element name=\"root\" type=\"tns:derived\"/>");
+    }
+
+    @Test
+    public void testExtendingAnEmptySequenceWithAnAllGroupWalks() {
+        assertAllGroupWalked("<xs:complexType name=\"base\"><xs:sequence/>"
+                             + "<xs:attribute name=\"id\" type=\"xs:string\"/>"
+                             + "</xs:complexType>"
+                             + "<xs:complexType name=\"derived\"><xs:complexContent>"
+                             + "<xs:extension base=\"tns:base\"><xs:all>"
+                             + "<xs:element name=\"a\" type=\"xs:string\"/>"
+                             + "</xs:all></xs:extension></xs:complexContent></xs:complexType>"
+                             + "<xs:element name=\"root\" type=\"tns:derived\"/>");
+    }
+
+    @Test
+    public void testExtendingANonEmptySequenceWithAnAllGroupIsRejected() {
+        try {
+            walkRoot("<xs:complexType name=\"base\"><xs:sequence>"
+                     + "<xs:element name=\"a\" type=\"xs:string\"/>"
+                     + "</xs:sequence></xs:complexType>"
+                     + "<xs:complexType name=\"derived\"><xs:complexContent>"
+                     + "<xs:extension base=\"tns:base\"><xs:all>"
+                     + "<xs:element name=\"b\" type=\"xs:string\"/>"
+                     + "</xs:all></xs:extension></xs:complexContent></xs:complexType>"
+                     + "<xs:element name=\"root\" type=\"tns:derived\"/>");
+            fail("expected an xs:all combined with other content to be rejected");
+        } catch (XmlSchemaException expected) {
+            assertTrue(expected.getMessage(), expected.getMessage().contains("xs:all"));
+        }
+    }
+
+    private static void assertAllGroupWalked(String body) {
+        List<String> events = walkEvents(body);
+        assertTrue(events.toString(), events.contains("onEnterAllGroup"));
+        assertFalse(events.toString(), events.contains("onEnterSequenceGroup"));
     }
 
     @Test
